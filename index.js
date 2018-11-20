51db32d144ae7e4919e49900d2de26ed582b0315
Ualdzimir Apenka <Uladzimir_Apenka@epam.com>
2018-11-20 13:32:24 +0300

    add MongoDB Cloud support

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const fs = require('fs');
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const jsonParser = bodyParser.json();
 
+const uri = "mongodb+srv://Uladzimir:211989206@cluster0-8fxtg.gcp.mongodb.net/test?retryWrites=true";
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
@@ -12,12 +15,17 @@ app.use(function(req, res, next) {
     next();
   });
 
-app.get("/", function(req, res){
-    const content = fs.readFileSync("data.json", "utf8");
-    res.send(content);
+app.get("/", function(req, res) {
+    MongoClient.connect(uri, (err, client) => {
+        assert.equal(null, err);
+        client.db('reactMMG').collection('score').find({}).toArray((err, score) => {
+            res.send(score);
+            client.close();
+        });
+    });
 });
 
-app.get("/log", function(req, res){
+app.get("/log", function(req, res) {
       
     const content = fs.readFileSync("server.log", "utf8");
 
@@ -25,17 +33,11 @@ app.get("/log", function(req, res){
 });
 
 app.post("/", jsonParser, function (req, res) {
-     
+    
     if (!req.body || !req.body.data || !req.body.data.score) {
         return res.sendStatus(400);
     }
-    
-    const data = fs.readFileSync('data.json', 'utf8');
-    const score = JSON.parse(data);
     const curScore = req.body;
-
-    
-    
     const username = (!curScore.data.username || curScore.data.username === '') ? 'Unknown Hero' : curScore.data.username;
     const email = (!curScore.data.email || curScore.data.email === '') ? 'Unknown' : curScore.data.email;
     const userScore = curScore.data.score;
@@ -44,15 +46,23 @@ app.post("/", jsonParser, function (req, res) {
         email: email,
         score: userScore,
     }
-    score.push(currentScore);
-    fs.writeFileSync('data.json', JSON.stringify(score));
-
-    res.send(JSON.stringify(score));
+    MongoClient.connect(uri, (err, client) => {
+        assert.equal(null, err);
+        client.db('reactMMG').collection('score').insertOne(currentScore)
+        .then(function(result) {
+            res.send('Data added');
+        });
+    });
 });
 
 app.delete("/", function(req, res){
-    fs.writeFileSync("data.json", JSON.stringify([]));
-    res.send('Deleting all data')
+    MongoClient.connect(uri, (err, client) => {
+        assert.equal(null, err);
+        client.db('reactMMG').collection('score').deleteMany({})
+        .then(function(result) {
+            res.send('Data deleted');
+        });
+    });
 });
 
 app.listen(PORT, (err) => {
